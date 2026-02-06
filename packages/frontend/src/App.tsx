90aecb88ce7306822e1df20efad264b7d5bb79ea
elberttimothy <elbert@autone.io>
2026-02-06 15:54:09 +0000

    connected FE to BE

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
 import { Button } from '@/atoms';
+import { useGetHealthQuery } from './store/api';
 
 function App() {
   const [count, setCount] = useState(0)
+  const { data, isLoading, error } = useGetHealthQuery()
+
+  console.log(data)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-8 p-8">
-      <h1 className="text-5xl font-bold bg-gradient-to-br from-primary-500 to-secondary-500 bg-clip-text text-transparent">
-        Autone Prototypes
-      </h1>
-      <div className="p-8">
-        <Button onClick={() => setCount((count) => count + 1)} aria-label="Increment count" id="increment-count">
-          Count is {count}
-        </Button>
+      <div className='flex flex-col'>
+        <h1>Health: {data?.status}</h1>
+        <p>Timestamp: {data?.timestamp}</p>
+        <p>Database: {data?.database}</p>
+        <p>Error: {data?.error}</p>
       </div>
-      <p className="text-gray-400 text-sm">
-        Edit <code className="bg-white/10 px-2 py-1 rounded font-mono">src/App.tsx</code> and save to test HMR
-      </p>
     </div>
   )
 }
--- packages/frontend/src/store/api.ts
+++ packages/frontend/src/store/api.ts
@@ -1,16 +1,23 @@
+import { HealthResponse } from '@autone/backend/schemas';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+
 
 // Define a base API slice with RTK Query
 // Add endpoints by extending this API using `api.injectEndpoints()`
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/api',
-    // Add default headers or credentials here if needed
-    // prepareHeaders: (headers) => {
-    //   return headers;
-    // },
+    baseUrl: 'http://localhost:3000/api',
   }),
   tagTypes: [],
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getHealth: builder.query<HealthResponse, void>({
+      query: () => '/health/test',
+      transformResponse: (response: HealthResponse) => response,
+    }),
+  }),
 });
+
+export const { useGetHealthQuery } = api;
